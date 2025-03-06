32b99f82cf040c0b292e0ce718244d649b1e23a7
Andres Romero <andresfromero328@gmail.com>
2025-03-06 14:56:30 -0800

    lazy render charts

--- app/(authenticated-content)/page.tsx
+++ app/(authenticated-content)/page.tsx
@@ -1,36 +1,14 @@
 import React from "react";
-import dynamic from "next/dynamic";
 
-const Spending = dynamic(() => import("@/components/dashboardComps/Spending"), {
-  ssr: false,
-});
-
-import Accounts from "@/components/dashboardComps/Accounts";
-import Budgets from "@/components/dashboardComps/Budgets";
-import Subscriptions from "@/components/dashboardComps/Subscriptions";
-// import Spending from "@/components/dashboardComps/Spending";
 import ContentLanding from "@/components/animations/ContentLanding";
-
-const COMPS = [
-  { components: Spending },
-  { components: Accounts },
-  { components: Budgets },
-  { components: Subscriptions },
-];
+import MainDashboard from "@/components/dashboardComps/MainDashboard";
 
 const HomePage = () => {
   return (
     <main id="main-container-dashboard" className="content ">
       <ContentLanding style="flex flex-col gap-2">
         <h1>Dashboard</h1>
-        {COMPS.map((Component, index) => (
-          <div
-            key={index}
-            className="flex flex-col gap-2 rounded-lg h-full w-full bg-special/50 shadow-onRest overflow-auto"
-          >
-            <Component.components />
-          </div>
-        ))}
+        <MainDashboard/>
       </ContentLanding>
     </main>
   );
